922f10c32550a253782c4911b123b15071ef4fd7
Pablo M. Reyes <pmreyes2@illinois.edu>
2016-11-24 19:40:28 -0600

    Adding .gitignore, __init__.py and bokeh_utils.py bokeh_utils.py has four definitions: get_all_palettes: Get all of matplotlib colormaps and translates them to palettes to use in bokeh. create_map_colorbar: Creates a Map and a Colorbar as two figures (this was before the latest version os bokeh included colorbar as a new feature) create_low_high_sliders: This creates a couple of sliders to manipulate the lower and upper limits of the palettes create_cmap_selection: Creates a selection tool to select and change the colormap used in on the figure map and colorbars.

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""Utility and application modules for web development
+
+bokeh_utils.py	--- functions useful when working with bokeh
+
+Typical usage:
+1) Getting all the matplotlib colormaps and translate it to bokeh format.
+
+
+"""
+
+__all__=['bokeh_utils','create_map_colorbar','create_low_high_sliders','create_cmap_selection']
