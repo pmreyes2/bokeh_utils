491e21111b7a83d88c496cf4019e21bb6a1941a3
Pablo M. Reyes <pmreyes2@illinois.edu>
2017-01-28 15:47:35 -0600

    breaking original bokeh_utils.py into different files. and adding valid_mpl_palettes()

--- __init__.py
+++ __init__.py
@@ -7,5 +7,10 @@ Typical usage:
 
 
 """
+from create_map_colorbar import create_map_colorbar
+from get_all_mpl_palettes import get_all_mpl_palettes
+from get_all_mpl_palettes import valid_mpl_palettes
+from create_cmap_selection import create_cmap_selection
+from create_low_high_sliders import create_low_high_sliders
 
-__all__=['bokeh_utils','create_map_colorbar','create_low_high_sliders','create_cmap_selection']
+__all__=['get_all_mpl_palettes','create_map_colorbar','valid_mpl_palettes','create_low_high_sliders','create_cmap_selection']
